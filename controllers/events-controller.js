143c85965de3ab63fc4757cdc9baee6e74b3c568
JohanGarzonF <johandanilogarzon@gmail.com>
2024-10-24 23:33:06 -0500

    Se modifica events controller para crear y leer eventos

--- controllers/events-controller.js
+++ controllers/events-controller.js
@@ -1,10 +1,14 @@
 const { response } = require('express')
 const Event = require('../models/event-model')
 
-const getEvents = ( req, res = response ) => {
+const getEvents = async ( req, res = response ) => {
+
+    const events = await Event.find()
+                            .populate('user', 'name')
+    
     return res.status(200).json({
         ok: true,
-        msg: 'Obtener eventos'
+        events
     })
 }
 
@@ -12,11 +16,12 @@ const createEvent = async ( req, res = response ) => {
     const event = new Event( req.body )
 
     try {
-        
-        await event.save()
+        event.user = req.uid
+        const saveEvent = await event.save()
 
         res.status(201).json({
             ok: true,
+            saveEvent,
             msg: 'Evento creado exitosamente'
         })
 
--- models/event-model.js
+++ models/event-model.js
@@ -25,4 +25,10 @@ const eventSchema = Schema({
 
 })
 
+eventSchema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject()
+    object.id = _id
+    return object
+})
+
 module.exports = model( 'event', eventSchema )
